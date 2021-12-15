4f9632eece9904bf7c7b92dfe8fac67661b9fc9d
Alfonsodela <89443967+Alfonsodela@users.noreply.github.com>
2021-12-15 20:45:54 +0100

    laSportiva

---
+++ src/app/index.js
@@ -0,0 +1,23 @@
+import './styles/styles.scss';
+import 'bootstrap';
+
+// function addListeners() {
+  TODO
+  // console.log('addListeners');
+// }
+
+window.onload = () => {
+  
+ 
+  const createContent = () => {
+    const ul = document.getElementById('hamburguer').classList.toggle('is-hidden');
+  }
+
+  const addListeners = () => {
+    const btn = document.getElementById('btn-add-content');
+    btn.addEventListener('click', 'createContent');
+  }
+};
+
+addListeners();
+
